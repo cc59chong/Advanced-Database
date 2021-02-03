12f07e6e1ce3b139cf3aa97f11419b70254b6352
chongchong6 <55506640+chongchong6@users.noreply.github.com>
2021-02-02 22:45:25 -0800

    Add files via upload

---
+++ cursors.sql
@@ -0,0 +1,105 @@
+﻿drop table if exists exams;
+create table exams(class_id int, whenithappens time, cheatsheet boolean);
+insert into exams values
+	(336, '1:00am', false),
+	(305, '6:00am', true),
+	(311, '12:00pm', false),
+	(318, '3:00pm', true);
+select * from exams;
+
+create or replace function feasible_tf() returns trigger as $$
+declare
+	examsCur cursor for select * from exams;
+begin
+	for erow in examsCur loop
+		if erow.whenithappens < NEW.whenithappens 
+			+ interval '2.5 hours' AND 
+			NEW.whenithappens < erow.whenithappens + '2.5 hours' then
+			raise notice 'second if %', erow;
+			return NULL;
+		end if;
+	end loop;
+	return NEW;
+end;
+$$ language plpgsql;
+
+create trigger feasible_t before insert on exams for each row
+	execute procedure feasible_tf();
+
+insert into exams values
+	(398, '9:00am', false),  --should be inserted.
+	(399, '12:02pm', false),
+	(399, '11:58am', false);
+
+select * from exams;
+
+create or replace function something(id integer) returns integer as $$
+declare
+	examsCur cursor(x int) for select * from exams where class_id = x;
+begin
+	--open examsCur;
+	for erow in examsCur(id) loop
+		if erow.cheatsheet then
+			update exams set cheatsheet = false where 
+				current of examsCur;
+		end if;
+		if erow.whenithappens <= '8:00am' then
+			delete from exams where current of examsCur;
+		end if;
+	end loop;
+	return 5;
+	--close examsCur;
+end;
+$$ language plpgsql;
+select something(318);
+
+create or replace function something2() returns integer as $$
+declare
+	examsCur cursor for select * from exams;
+	erow exams%rowtype;
+begin
+	open examsCur;
+	fetch examsCur into erow;
+	while found loop
+		raise notice 'row is %', erow;	
+		fetch examsCur into erow;
+	end loop;	
+	close examsCur;
+	return 5;
+end;
+$$ language plpgsql;
+
+select something2();
+--select something2();
+
+create or replace function something3() returns integer as $$
+declare
+	examsCur cursor for select * from exams;
+	erow exams%rowtype;
+begin
+	--open examsCur;
+	for erow in (select * from exams as a,exams) loop
+		raise notice 'row is %', erow;	
+	end loop;
+	return 5;
+	--close examsCur;
+end;
+$$ language plpgsql;
+
+select something3();
+
+create or replace function something4() returns integer as $$
+declare
+	examsCur cursor for (select * from exams as a,exams);
+--	erow exams%rowtype;
+begin
+	--open examsCur;
+	for erow in examsCur loop
+		raise notice 'row is %', erow;	
+	end loop;
+	return 5;
+	--close examsCur;
+end;
+$$ language plpgsql;
+
+select something4();
