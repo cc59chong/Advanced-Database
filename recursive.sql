83dfe03373293def9035d7c92b29f80dc8367d1a
chongchong6 <55506640+chongchong6@users.noreply.github.com>
2021-02-04 13:06:33 -0800

    Add files via upload

---
+++ recursive.sql
@@ -0,0 +1,25 @@
+﻿with recursive t(n) as ( values(1)
+     union all
+     select n + 1 from t where n < 10)
+select sum(n) from t;
+
+
+create table graph( 
+   interm int,
+   outerm int
+);
+
+insert into graph values
+    (1,2),
+    (2,3),
+    (3,1),
+    (4,5);
+
+with recursive reachable(n) as (
+     values(1)
+     union
+     select outerm from reachable, graph
+           where graph.interm = n
+)
+
+select n from reachable;
