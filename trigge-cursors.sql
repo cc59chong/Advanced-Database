d148c4bc5079bf971860df8701d08bae12982b94
chongchong6 <55506640+chongchong6@users.noreply.github.com>
2021-02-04 12:59:50 -0800

    Add files via upload

---
+++ trigge-cursors.sql
@@ -0,0 +1,109 @@
+﻿drop table if exists tt;
+create table tt(
+       a int primary key,
+       b text
+
+);
+
+/*
+create or replace function nodups() returns trigger as $$
+begin
+      if (select a from tt where New.a =a) is null
+            then return NEW;
+      else return null;
+      end if;
+end;
+$$ language plpgsql;
+*/
+
+/*
+create or replace function nodups() returns trigger as $$
+begin
+      if (select a from tt where New.a =a) is null
+            then raise notice 'inserting%.', NEW;
+            return NEW;
+      else raise notice 'not inserting%.', NEW;
+            return null;
+      end if;
+end;
+$$ language plpgsql;
+*/
+
+create or replace function nodups() returns trigger as $$
+begin
+      if (select a from tt where New.a =a) is null
+            then raise notice 'inserting%.', NEW;
+            return NEW;
+      else  
+            NEW.a := NEW.a + 1;
+            insert into tt values (NEW.a, NEW.b);
+            raise notice 'trying to insert %.', NEW;
+            return null;
+      end if;
+end;
+$$ language plpgsql;
+
+create trigger nodupstrig before insert on tt for each row 
+       execute procedure nodups();
+
+
+create or replace function nodel() returns trigger as $$
+begin
+         return null;
+end;
+$$ language plpgsql;
+
+create trigger nodeletes before delete on tt for each row 
+       execute procedure nodel();
+       
+ 
+/*
+create trigger nodeletes after delete on tt for each row 
+       execute procedure nodel();u
+ */
+
+create or replace function upchz() returns trigger as $$
+begin
+      if OLD.b = 'apples' AND NEW.b = 'oranges' then
+            NEW.b := 'cheese';
+            return NEW;
+      else
+            return null;
+      end if;
+end;
+$$ language plpgsql; 
+
+create or replace function sumToN(n int) returns int as $$
+declare   
+        x int;
+        ans int;
+begin
+        x := 1;
+        ans := 0;
+        loop
+                ans := ans + x;
+                x := x + 1;
+               -- exit when x = n + 1;
+                exit when x = n;
+                x := x + 1;
+        end loop;
+        return ans;
+end;
+$$ language plpgsql; 
+
+
+create trigger upchztrig before delete on tt for each row 
+       execute procedure upchz();
+
+insert into tt values (1, 'cheese');
+insert into tt values (1, 'chalk');
+insert into tt values (1, 'apples');
+insert into tt values (3, 'apples');
+
+delete from tt where a=3;
+
+update tt set b = 'oranges' where b= 'apples';
+--update tt set b = 'apples' where b= 'cheese';
+
+select * from tt;
+select sumToN(5);
---
+++ trigger-1.sql
@@ -0,0 +1,107 @@
+﻿drop table if exists customers;
+drop table if exists salespersons;
+
+create table salespersons
+(
+     id integer primary key,
+     firstname text
+);
+
+create table customers(
+     firstname text primary key,
+     id integer references salespersons(id)
+);
+
+insert into salespersons values
+        (1, 'Sam'),
+        (2, 'Sally'),
+        (3, 'Stephen');
+
+drop function if exists insertnotice();
+create function insertnotice() returns trigger as $$
+begin
+        raise notice '% just inserted!', NEW.firstname;
+        return NEW; -- Always return new!
+end
+$$ language plpgsql;
+
+drop function if exists assignSalesFunc();
+create function assignSalesFunc() returns trigger as $$ 
+begin
+       NEW.id := (select salespersons.id from customers right outer join salespersons
+        on salespersons.id = customers.id group by 
+        salespersons.id having count(customers.firstname) = 
+        (select min(count) from
+                (select count(customers.firstname) from 
+                 salespersons left outer join customers
+                 on salespersons.id = customers.id group by
+                 salespersons.id) as t) limit 1);
+        return NEW;   
+end;
+$$ language plpgsql;
+create trigger assignSales before insert on customers for each row
+       execute procedure assignSalesFunc(); 
+
+-- trigger as constraint.
+-- salesperson Sauruman is bad, we don't want to add him to the table.
+
+drop function if exists badsalesman();
+create function badsalesman() returns trigger as $$
+begin
+        if NEW.firstname = 'Sauruman' then
+               raise exception 'Sauruman must not be added.';
+               return null;
+        end if;
+        return null;
+end;
+$$ language plpgsql;
+
+create trigger bs after insert on salespersons for each row 
+        execute procedure badsalesman();
+
+
+insert into customers values
+        ('Chris', null),
+        ('Chrelsea', null),
+        ('Christian', null),
+        ('Chloe', null),
+        ('Clifford', null),
+        ('Clark', null);
+
+ select * from customers;
+
+drop function if exists redistributeSalespeopleFunc();
+create function redistributeSalespeopleFunc() returns trigger as $$
+declare
+spcur scroll cursor(sid integer) for select * from
+        salespersons where id != sid;
+customersOf cursor(sid integer) for select  * from customers where id = sid;
+salesperson record;
+customer record;
+begin
+       open spcur(OLD.id);
+       for customer in customersOf(OLD.id) loop
+             fetch spcur into salesperson;
+             if not found then
+                     fetch first from spcur into salesperson;
+             end if;
+             update customers set id = salesperson.id where
+                    current of customersOf;
+                    
+       end loop;
+       close spcur;
+       return OLD;
+end;
+$$ language plpgsql;
+
+create trigger redistributesalespeople before delete on salespersons
+       for each row
+       execute procedure redistributeSalespeopleFunc();
+
+delete from salespersons where id =1;
+delete from salespersons where id =2;
+
+--insert into salespersons values (4, 'Sauruman');
+
+--select * from salespersons;
+ 
